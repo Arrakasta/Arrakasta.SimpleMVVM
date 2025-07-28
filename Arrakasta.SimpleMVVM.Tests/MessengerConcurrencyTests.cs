6725dddaa7955fd68109147f608a400d4efae852
Alexey Nis <alexey.nishluk@gmail.com>
2025-07-28 15:42:31 +0300

    Make Messenger thread-safe

---
+++ Arrakasta.SimpleMVVM.Tests/MessengerConcurrencyTests.cs
@@ -0,0 +1,40 @@
+using Arrakasta.SimpleMVVM.Messengers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Arrakasta.SimpleMVVM.Tests;
+
+public class MessengerConcurrencyTests
+{
+    [Fact]
+    public async Task Concurrent_Send_ShouldDeliverMessages()
+    {
+        var messenger = new Messenger();
+        int received = 0;
+        messenger.Subscribe<string>(_ => Interlocked.Increment(ref received));
+
+        var tasks = Enumerable.Range(0, 100).Select(_ => Task.Run(() => messenger.Send("msg")));
+        await Task.WhenAll(tasks);
+
+        Assert.Equal(100, received);
+    }
+
+    [Fact]
+    public async Task Concurrent_Subscribe_Unsubscribe_Send_ShouldBeThreadSafe()
+    {
+        var messenger = new Messenger();
+        int received = 0;
+
+        var tasks = Enumerable.Range(0, 100).Select(_ => Task.Run(() =>
+        {
+            Action<string> handler = _ => Interlocked.Increment(ref received);
+            messenger.Subscribe(handler);
+            messenger.Send("test");
+            messenger.Unsubscribe(handler);
+        }));
+
+        await Task.WhenAll(tasks);
+
+        Assert.True(received >= 100);
+    }
+}
